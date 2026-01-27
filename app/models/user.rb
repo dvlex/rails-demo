382d69f79d03105382e95465d62653e7e19afeed
Alex Castillo <alex@lexdrel.com>
2026-01-27 17:09:16 +0000

    UIUX improvements and user linkedin data retrieval

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,12 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.title = auth.info.description
+      user.photo_url = auth.info.picture_url
+      user.location = auth.info.location
+      user.linkedinurl = auth.info.urls&.dig("public_profile")
     end
   end
 end
