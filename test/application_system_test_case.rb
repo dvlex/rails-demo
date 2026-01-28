92272b95254cb43df25917a556a5af2d82539fd0
Alex Castillo <alex@lexdrel.com>
2026-01-28 19:42:33 +0000

    adapt to google chrome in ci environments

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,6 +4,10 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_option|
     driver_option.add_argument("--no-sandbox")
     driver_option.add_argument("--disable-dev-shm-usage")
-    driver_option.binary = "/usr/bin/chromium" if File.exist?("/usr/bin/chromium")
+    if ENV["CI"] && File.exist?("/usr/bin/google-chrome")
+      driver_option.binary = "/usr/bin/google-chrome"
+    elsif !ENV["CI"] && File.exist?("/usr/bin/chromium")
+      driver_option.binary = "/usr/bin/chromium"
+    end
   end
 end
