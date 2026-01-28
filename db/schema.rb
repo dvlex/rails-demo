bc7ae20905e6d19a08f610cddad39c6ce183663b
Alex Castillo <alex@lexdrel.com>
2026-01-28 17:47:52 +0000

    rename linkedin_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_27_164659) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_28_172647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,7 +51,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_27_164659) do
     t.string "first_name"
     t.string "title"
     t.string "location"
-    t.string "linkedinurl"
+    t.string "linkedin_url"
     t.string "photo_url"
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
