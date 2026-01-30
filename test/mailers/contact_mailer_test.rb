058c5fbc4b0182ae0b121775315b36daa4903f60
Alex Castillo <alex@lexdrel.com>
2026-01-30 23:08:53 +0000

    create email response to contact form

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
-class TestMailerTest < ActionMailer::TestCase
-  test "welcome_email" do
-    mail = TestMailer.welcome_email
-    assert_equal "Welcome email", mail.subject
+class ContactMailerTest < ActionMailer::TestCase
+  test "new_contact_form" do
+    mail = ContactMailer.new_contact_form
+    assert_equal "New contact form", mail.subject
     assert_equal [ "to@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
     assert_match "Hi", mail.body.encoded
