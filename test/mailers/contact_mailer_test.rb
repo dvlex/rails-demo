97050ec22604af02149f285362e679efd4f4cee2
Alex Castillo <alex@lexdrel.com>
2026-01-31 17:49:50 +0000

    fix specs

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class ContactMailerTest < ActionMailer::TestCase
   test "new_contact_form" do
-    mail = ContactMailer.new_contact_form
-    assert_equal "New contact form", mail.subject
+    mail = ContactMailer.new_contact_form("to@example.org", "Test User")
+    assert_equal "Thank you for contacting us", mail.subject
     assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [ "noreply@lexdrel.com" ], mail.from
+    assert_match "Thank you for getting in touch with us through Lexdrel.com.", mail.body.encoded
   end
 end
